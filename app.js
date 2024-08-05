fbbd372e10446b0e032479a35ab282ae6c424858
rohit thakur <rohit.thakur@tristatetechnology.co.in>
2024-08-05 11:59:56 +0530

    revert the third commit

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ export default function app(){
         <div>
             <p>git demo</p>
             <p>adding more line</p>
-            <p>add and commit in one line</p>
+            <p>adding more line</p>
+            <p>adding more line</p>
+            <p>adding more line</p>
         </div>
     )
 }
