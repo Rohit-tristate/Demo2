9bc48d649ba93452e691de830c3d0b0957eb5634
rohit thakur <rohit.thakur@tristatetechnology.co.in>
2024-08-05 12:21:31 +0530

    removing first  p tag

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 export default function app(){
     return(
         <div>
-            <p>git demo</p>
             <p>adding more line</p>
             <p>adding more line</p>
             <p>adding more line</p>
