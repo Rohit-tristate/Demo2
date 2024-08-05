e81ce779840330745b224c5e4748fcf050a38b55
rohit thakur <rohit.thakur@tristatetechnology.co.in>
2024-08-05 12:23:36 +0530

    removing 3 lines

--- app.js
+++ app.js
@@ -2,10 +2,7 @@ export default function app(){
     return(
         <div>
             <p>git demo</p>
-            <p>adding more line</p>
-            <p>adding more line</p>
-            <p>adding more line</p>
-            <p>adding more line</p>
+            
         </div>
     )
 }
